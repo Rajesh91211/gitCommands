57a2dd398d72868e47cc3838b05ab179df84cc81
Rajesh91211 <rajeshtangellamudi329@gmail.com>
2025-11-21 15:55:48 +0530

    new line added

--- Commands/src/gitcommands/Commandspractice.java
+++ Commands/src/gitcommands/Commandspractice.java
@@ -7,6 +7,7 @@ public class Commandspractice {
 System.out.println("hello world");
 System.out.println("learning git commands");
 System.out.println("with eclipse ide");
+System.out.println("Continuing the topics..");
 	}
 
 }
