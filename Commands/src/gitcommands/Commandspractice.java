6e4918037d5392a301136c5d5ad90af52d696d39
Rajesh91211 <rajeshtangellamudi329@gmail.com>
2025-11-21 13:44:28 +0530

    intial commit to new print

--- Commands/src/gitcommands/Commandspractice.java
+++ Commands/src/gitcommands/Commandspractice.java
@@ -5,6 +5,7 @@ public class Commandspractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
+System.out.println("learning git commands");
 	}
 
 }
